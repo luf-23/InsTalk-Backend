a7f53337b8510101c331d3342103e5660cc99dd9
yzj_luf <3162794813@qq.com>
2025-09-28 21:29:34 +0800

    初始化

---
+++ src/main/java/org/instalkbackend/InsTalkBackendApplication.java
@@ -0,0 +1,13 @@
+package org.instalkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsTalkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsTalkBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/instalkbackend/InsTalkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.instalkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsTalkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
